3073cc4dceb4fed8899dea852765670dedad6f7d
Brandon Smylie <brandon.smylie@unosquare.com>
2025-03-19 15:30:35 -0400

    Auth changes.

--- controllers/albums.go
+++ controllers/albums.go
@@ -1,66 +1,30 @@
 package controllers
 
 import (
+	"goMusic/authentication"
 	"goMusic/services"
 	"net/http"
-	"regexp"
 	"strconv"
 )
 
 func RegisterAlbumRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/albums", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			GetAlbums(w, r)
-		case http.MethodPost:
-			PostAlbum(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+	mux.HandleFunc("GET /albums", services.GetAlbums)
+	mux.HandleFunc("GET /albums/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(r.PathValue("id"))
+		services.GetAlbumByID(w, r, id)
 	})
 
-	albumIDPattern := regexp.MustCompile(`^/albums/(.+)$`)
-	mux.HandleFunc("/albums/", func(w http.ResponseWriter, r *http.Request) {
-		matches := albumIDPattern.FindStringSubmatch(r.URL.Path)
-		if matches == nil || len(matches) < 2 {
-			http.NotFound(w, r)
-			return
-		}
-		id := matches[1]
-		num, err := strconv.Atoi(id)
-		if err != nil {
-			http.Error(w, "Invalid album ID", http.StatusBadRequest)
-			return
-		}
-		switch r.Method {
-		case http.MethodGet:
-			GetAlbumByID(w, r, num)
-		case http.MethodPut:
-			UpdateAlbumByID(w, r, num)
-		case http.MethodDelete:
-			DeleteAlbumByID(w, r, num)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-}
-
-func GetAlbums(w http.ResponseWriter, r *http.Request) {
-	services.GetAlbums(w, r)
-}
-
-func GetAlbumByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.GetAlbumByID(w, r, id)
-}
-
-func PostAlbum(w http.ResponseWriter, r *http.Request) {
-	services.PostAlbum(w, r)
-}
-
-func UpdateAlbumByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.UpdateAlbumByID(w, r, id)
-}
-
-func DeleteAlbumByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.DeleteAlbumByID(w, r, id)
+	mux.HandleFunc("POST /albums", authentication.AuthMiddleware(services.PostAlbum))
+	mux.HandleFunc("PUT /albums/{id}", authentication.AuthMiddleware(
+		func(w http.ResponseWriter, r *http.Request) {
+			id, _ := strconv.Atoi(r.PathValue("id"))
+			services.UpdateAlbumByID(w, r, id)
+		},
+	))
+	mux.HandleFunc("DELETE /albums/{id}", authentication.AuthMiddleware(
+		func(w http.ResponseWriter, r *http.Request) {
+			id, _ := strconv.Atoi(r.PathValue("id"))
+			services.DeleteAlbumByID(w, r, id)
+		},
+	))
 }
--- controllers/bands.go
+++ controllers/bands.go
@@ -1,66 +1,30 @@
 package controllers
 
 import (
+	"goMusic/authentication"
 	"goMusic/services"
 	"net/http"
-	"regexp"
 	"strconv"
 )
 
 func RegisterBandRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/bands", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			GetBands(w, r)
-		case http.MethodPost:
-			PostBand(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+	mux.HandleFunc("GET /bands", services.GetBands)
+	mux.HandleFunc("GET /bands/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(r.PathValue("id"))
+		services.GetBandByID(w, r, id)
 	})
 
-	bandIDPattern := regexp.MustCompile(`^/bands/(.+)$`)
-	mux.HandleFunc("/bands/", func(w http.ResponseWriter, r *http.Request) {
-		matches := bandIDPattern.FindStringSubmatch(r.URL.Path)
-		if matches == nil || len(matches) < 2 {
-			http.NotFound(w, r)
-			return
-		}
-		id := matches[1]
-		num, err := strconv.Atoi(id)
-		if err != nil {
-			http.Error(w, "Invalid band ID", http.StatusBadRequest)
-			return
-		}
-		switch r.Method {
-		case http.MethodGet:
-			GetBandByID(w, r, num)
-		case http.MethodPut:
-			UpdateBandByID(w, r, num)
-		case http.MethodDelete:
-			DeleteBandByID(w, r, num)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-}
-
-func GetBands(w http.ResponseWriter, r *http.Request) {
-	services.GetBands(w, r)
-}
-
-func GetBandByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.GetBandByID(w, r, id)
-}
-
-func PostBand(w http.ResponseWriter, r *http.Request) {
-	services.PostBand(w, r)
-}
-
-func UpdateBandByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.UpdateBandByID(w, r, id)
-}
-
-func DeleteBandByID(w http.ResponseWriter, r *http.Request, id int) {
-	services.DeleteBandByID(w, r, id)
+	mux.HandleFunc("POST /bands", authentication.AuthMiddleware(services.PostBand))
+	mux.HandleFunc("PUT /bands/{id}", authentication.AuthMiddleware(
+		func(w http.ResponseWriter, r *http.Request) {
+			id, _ := strconv.Atoi(r.PathValue("id"))
+			services.UpdateBandByID(w, r, id)
+		},
+	))
+	mux.HandleFunc("DELETE /bands/{id}", authentication.AuthMiddleware(
+		func(w http.ResponseWriter, r *http.Request) {
+			id, _ := strconv.Atoi(r.PathValue("id"))
+			services.DeleteBandByID(w, r, id)
+		},
+	))
 }
